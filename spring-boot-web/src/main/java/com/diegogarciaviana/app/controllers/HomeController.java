16c670e2bb7f1de5415a5179308208da65944afa
DGV1995 <diegogarciaviana.1995@gmail.com>
2019-06-14 12:09:32 +0200

    Curso Spring Framework

--- spring-boot-web/src/main/java/com/diegogarciaviana/app/controllers/HomeController.java
+++ spring-boot-web/src/main/java/com/diegogarciaviana/app/controllers/HomeController.java
@@ -9,8 +9,8 @@ public class HomeController {
 	@GetMapping("/")
 	public String home() {
 		// Este método redirige la aplicación a la vista indicada después de la palabra clase 'redirect'
-		//return "redirect:/app/index";
-		return "forward:/app/index"; // Igual, pero sin reiniciar los datos del request
+		//return "redirect:/app/index"; // Cambia la ruta url
+		return "forward:/app/index"; // Igual, pero sin reiniciar los datos del request (la ruta url se mantiene en la que estaba) ==> no se puede hacer un forward a una página externa de la aplicación (como google.es)
 		//return "redirect:https://www.google.es";
 	}
 
